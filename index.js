f3116401c62823971a625b2a1d6ea4625f0a87bb
Ryosuke Maruyama <ryosuke.uitec@gmail.com>
2019-07-31 16:14:01 +0900

    Make to execute command of args when coming request

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const http = require('http');
-const { spawn } = require('child_process');
+const { spawn, execSync } = require('child_process');
+
+if (process.argv[2] === undefined) {
+  console.log('Please input command that run.');
+  return
+}
+const cmd = process.argv[2];
 
 // Proxy a local server to Serveo.net
 const serveo = spawn('ssh', ['-R', '80:localhost:8080', 'serveo.net']);
 serveo.stdout.on('data', (data) => {
-  console.log(`stdout: ${data}`);
+  console.log(`serveo: ${data}`);
 });
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
+  const stdout = execSync(cmd).toString('utf8');
+  console.log(stdout);
   res.end('OK');
 })
 
